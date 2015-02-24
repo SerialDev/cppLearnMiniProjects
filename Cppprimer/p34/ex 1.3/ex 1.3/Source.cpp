b624cc221ef683a6b82c9f696c8bdb556577e029
CAMMari <carlos.mariscal.melgar@gmail.com>
2015-02-24 21:26:17 +0000

    cpp primer
    __________________________________________________
    Sequential work through C++ Primer 5th ed.
    part of learning C++

---
+++ Cppprimer/p34/ex 1.3/ex 1.3/Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() 
+{
+	std::cout << "Hello World!" << std::endl;
+
+	
+	std::cout << "Press any key to continue..." << std::endl;
+	std::cin.get();
+}
---
+++ Cppprimer/p34/ex 1.4/ex 1.4/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+
+int main()
+{
+	std::cout << "Enter two numbers: " << std::endl;
+	int x = 0, y = 0;
+	std::cin >> x >> y;
+	std::cout << "The product of " << x << " and " << y << " is " << x * y << std::endl;
+
+	std::cout << "Press any key...";
+	std::cin.ignore();
+	std::cin.get();
+
+	
+}
